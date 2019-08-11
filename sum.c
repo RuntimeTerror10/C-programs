bd3ee0dfc5b45d6b81261277d1a309176eb35877
Parth Bhardwaj <parth@LAPTOP-3JQMM2OE.localdomain>
2019-08-11 09:23:23 +0530

    here is a code for adding two numbers using a function

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int sum(int a ,int b){
+    return a+b;
+}
+
+int main(){
+int i,j,add;
+printf("enter first number\n");
+scanf("%d",&i);
+printf("enter second number\n");
+scanf("%d",&j);
+printf("sum of above two numbers is: %d",add=sum(i,j));
+
+
+}
