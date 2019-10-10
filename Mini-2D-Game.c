d44a8851874a214249abce3de0ac7a535a668315
Parth Bhardwaj <parth@LAPTOP-3JQMM2OE.localdomain>
2019-10-10 17:58:58 +0530

    added function to print grid and indentation

--- Mini-2D-Game.c
+++ Mini-2D-Game.c
@@ -4,6 +4,7 @@
 int main()
 {   int x=0,y=0;
     char direction;
+    int i,j,k;
     int grid[10][10]={
                         {1,0,0,0,0,0,0,0,0,0},
                         {0,0,0,0,0,0,0,0,0,0},
@@ -15,9 +16,9 @@ int main()
                         {0,0,0,0,0,0,0,0,0,0},
                         {0,0,0,0,0,0,0,0,0,0},
                         {0,0,0,0,0,0,0,0,0,0},
-                        };
-        int i,j,k;
-        for(i=0;i<10;i++){
+    };
+    void print_grid(){
+       for(i=0;i<10;i++){
             for(j=0;j<10;j++){
                 if(grid[i][j]==1){
                     printf("%c",grid[i][j]='x');
@@ -27,84 +28,55 @@ int main()
                 }
             }
 
-        printf("\n");
+                printf("\n");
         }
 
-        for(k=0;;k++){
-            direction=getch();
-            if(direction=='s'){
-              grid[x][y]=0;
-              x++;
-              grid[x][y]=1;
-              system("cls");
-              for(i=0;i<10;i++){
-                for(j=0;j<10;j++){
-                    if(grid[i][j]==1){
-                        printf("%c",grid[i][j]='x');
-                    }
-                    else{
-                    printf("%c",grid[i][j]='.');
-                    }
-                }
+    }
 
-                printf("\n");
-                }
-
+    for(i=0;i<10;i++){
+        for(j=0;j<10;j++){
+            if(grid[i][j]==1){
+                printf("%c",grid[i][j]='x');
             }
-            else if(direction=='d'){
-              grid[x][y]=0;
-              y++;
-              grid[x][y]=1;
-              system("cls");
-              for(i=0;i<10;i++){
-                for(j=0;j<10;j++){
-                    if(grid[i][j]==1){
-                        printf("%c",grid[i][j]='x');
-                    }
-                    else{
+            else{
                     printf("%c",grid[i][j]='.');
-                    }
-                }
-
-                printf("\n");
-                }
             }
-            else if(direction=='a'){
-               grid[x][y]=0;
-               y--;
-               grid[x][y]=1;
-               system("cls");
-               for(i=0;i<10;i++){
-                for(j=0;j<10;j++){
-                    if(grid[i][j]==1){
-                        printf("%c",grid[i][j]='x');
-                    }
-                    else{
-                    printf("%c",grid[i][j]='.');
-                    }
-                }
+        }
 
-                printf("\n");
-                }
-            }
-            else if(direction=='w'){
-               grid[x][y]=0;
-               x--;
-               grid[x][y]=1;
-               system("cls");
-               for(i=0;i<10;i++){
-                for(j=0;j<10;j++){
-                    if(grid[i][j]==1){
-                        printf("%c",grid[i][j]='x');
-                    }
-                    else{
-                    printf("%c",grid[i][j]='.');
-                    }
-                }
+    printf("\n");
+    }
 
-                printf("\n");
-                }
-            }
+    for(k=0;;k++){
+        direction=getch();
+        if(direction=='s'){
+            grid[x][y]=0;
+            x++;
+            grid[x][y]=1;
+            system("cls");
+            print_grid();
+
+        }
+        else if(direction=='d'){
+            grid[x][y]=0;
+            y++;
+            grid[x][y]=1;
+            system("cls");
+            print_grid();
+        }
+        else if(direction=='a'){
+            grid[x][y]=0;
+            y--;
+            grid[x][y]=1;
+            system("cls");
+            print_grid();
+        }
+        else if(direction=='w'){
+            grid[x][y]=0;
+            x--;
+            grid[x][y]=1;
+            system("cls");
+            print_grid();
+        }
 
         }
 
